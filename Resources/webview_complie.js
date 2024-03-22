13baa805c93e2f41721a9360cb8ed38884c949cd
asurgingriver <aSurgingRiver@163.com>
2024-03-22 09:22:28 +0800

    5.0
    2024/3/22
    1. 删除SetVisibility函数的中对暂停渲染调用
    2. 增加stoprender接口暂停网页渲染
    3. 优化渲染性能，减少GPU计算和显存消耗
    4. 优化企业版渲染流程，减少渲染延迟
    5. 优化内存释放，减少内存使用

---
+++ Resources/webview_complie.js
@@ -0,0 +1,5 @@
+
+"object"!=typeof ue && (ue={});
+"object"!=typeof ue.interface && (ue.interface={});
+"function"==typeof ue.call&&(ue.call=(k,j,c,t)=>{console.log('used for compilation');});
+"function"==typeof ue4&&(ue4=ue.call);
