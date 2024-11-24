761ac7aeb59d9e39f476fc86e241516a0a2267f4
asurgingriver <aSurgingRiver@163.com>
2024-11-24 21:09:17 +0800

    5.4
    2024/11/24
    1. 优化企业版GPU渲染
    2. 增加网页加载失败错误转换
    3. 支持网页title标签提示

--- Source/WebView/Private/WebBase.cpp
+++ Source/WebView/Private/WebBase.cpp
@@ -179,7 +179,7 @@ void UWebBase::BeginDestroy() {
 	if (WebWidget) {
 		WebWidget->Close();
 		WebWidget->SetCanTick(false);
-		WebWidget.Reset();
+		WebWidget = nullptr ;
 	}
 	Super::BeginDestroy();
 }
@@ -362,6 +362,7 @@ void UWebBase::ReleaseSlateResources(bool bReleaseChildren) {
 	if (WebWidget) {
 		WebWidget->StopRender(false);
 		WebWidget->Close();
+		WebWidget = nullptr;
 	}
 	if (_ViewObject)_ViewObject = nullptr;
 }
