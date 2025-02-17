2d382e602d3eb9267a6a5fe74563a19f5e411b83
asurgingriver <aSurgingRiver@163.com>
2025-02-17 13:08:33 +0800

    5.4
    2025/2/17
    1. 解决蓝图结构体转Json对象时的名称不对问题
    2. 同步Windows平台下CEF头文件
    3. 优化编辑器停止运行时的内存清理。
    4. 优化cefhelper 兼容在JS中使用ue4脚本调用UE

--- Source/ThirdParty/cefForUe/cefForUe.build.cs
+++ Source/ThirdParty/cefForUe/cefForUe.build.cs
@@ -20,6 +20,7 @@ public class cefForUe : ModuleRules
         if (Target.Platform == UnrealTargetPlatform.Win64) {
             //InitCEF3_Win("cef_103.5060");
             InitCEF3_Win("cef_120.6099");
+            //InitCEF3_Win("cef_122.6261");
             //InitCEF3_Win("cef_129.6668");
             //InitCEF3_Win("cef_115.5790");
         }
