ba61878238b1fc3e26c44ced64498f88101493fe
asurgingriver <aSurgingRiver@163.com>
2025-05-11 17:26:10 +0800

    5.4
    2025/5/10
    1. 解决Linux下键盘按键失效问题。
    2. 增加international_keyboards=true 参数，用于国际键盘启用Ctr+ALT 功能

--- Source/ThirdParty/CefBrowser/include/SCefBrowser.h
+++ Source/ThirdParty/CefBrowser/include/SCefBrowser.h
@@ -130,6 +130,7 @@ public:
 	void Construct(const FArguments& InArgs,const bool isFirst=true);
 private:
 	//
+	virtual int32 OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const override;
 	virtual void Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime) override;
 	virtual FReply OnDragDetected(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
 	virtual void OnDragEnter(const FGeometry& MyGeometry, const FDragDropEvent& DragDropEvent) override;
